aa2d1a373b9599aeefd464723dfa44077cfa946d
AKANE_CHAN <taiseinanai@gmail.com>
2021-01-07 02:14:47 +0900

    EnemyB(射撃型)のSE差し込み ・足音 ・射撃音 ・撃破音 を実装

--- Assets/AudioManager/Scripts/SEPath.cs
+++ Assets/AudioManager/Scripts/SEPath.cs
@@ -5,9 +5,13 @@
 /// </summary>
 public static class SEPath{
 
-	public const string JINGLE10 = "SE/Jingle/jingle10";
-	public const string SYSTEM20 = "SE/system20";
-	public const string SYSTEM23 = "SE/system23";
+	public const string ENEMY_ARREST = "SE/EnemyArrest/enemy_Arrest";
+	public const string ENEMY_SHOOT2 = "SE/EnemyB_SE/enemy_Shoot2";
+	public const string JINGLE10     = "SE/Jingle/jingle10";
+	public const string RUN1         = "SE/EnemyB_SE/run1";
+	public const string RUN2         = "SE/EnemyB_SE/run2";
+	public const string SYSTEM20     = "SE/system20";
+	public const string SYSTEM23     = "SE/system23";
 
 }
 
--- Assets/Nanai/Script/EnemyLife.cs
+++ Assets/Nanai/Script/EnemyLife.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KanKikuchi.AudioManager;
 
 public class EnemyLife : MonoBehaviour
 {
@@ -19,12 +20,12 @@ public class EnemyLife : MonoBehaviour
     {
         if (Life <= 0)
         {
-            //if (true)
-            {
-                Instantiate(particleObject, this.transform.position, Quaternion.Euler(-90f,0f,0f));
-                ManagerScript.AddCount();
-                Destroy(this.gameObject);
-            }
+            //捕獲パーティクル生成
+            Instantiate(particleObject, this.transform.position, Quaternion.Euler(-90f,0f,0f));
+            //捕獲SE再生
+            SEManager.Instance.Play(SEPath.ENEMY_ARREST, 0.3f, 0, 1, false, null);
+            ManagerScript.AddCount();
+            Destroy(this.gameObject);
         }
     }
     void OnCollisionEnter(Collision other)
