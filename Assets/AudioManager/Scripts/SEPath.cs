6108bfc2d2030278fea166ac1a727378152727a8
Cysa8010 <hishoslash@gmail.com>
2021-01-07 01:26:18 +0900

    なんか色々いじった

--- Assets/AudioManager/Scripts/SEPath.cs
+++ Assets/AudioManager/Scripts/SEPath.cs
@@ -5,9 +5,10 @@
 /// </summary>
 public static class SEPath{
 
-	public const string JINGLE10 = "SE/Jingle/jingle10";
-	public const string SYSTEM20 = "SE/system20";
-	public const string SYSTEM23 = "SE/system23";
+	public const string EQUIP1   = "SE/Menu/equip1";
+	public const string JINGLE10 = "SE/Default/Jingle/jingle10";
+	public const string SYSTEM20 = "SE/Default/system20";
+	public const string SYSTEM23 = "SE/Default/system23";
 
 }
 
