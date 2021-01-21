4023aa150f319118129aebb05acba7ccfec418b8
Cysa8010 <hishoslash@gmail.com>
2021-01-21 16:34:32 +0900

    UIスクリプト修正、その他諸々

--- Assets/Cysa/RockOn/LockOnSystem.cs
+++ Assets/Cysa/RockOn/LockOnSystem.cs
@@ -7,12 +7,12 @@ using System.Linq;
 public class LockOnSystem : MonoBehaviour
 {
 	//public GameObject enemyUnit;
-	[SerializeField] private string searchTag = "Enemy";
+	[SerializeField] private string searchTag = null;
 	public int lockOnTime = 3;
 	private int elapsedTime = 0;
 	private const int MAX_LOCK_ON_TIME = 3600;
 	private bool isLockOn = false;
-	public float lockOnCircle = 150;
+	//public float lockOnCircle = 150;
 
 	[SerializeField] private GameObject player = null;
 	[SerializeField] private GameObject weapon;
@@ -53,12 +53,13 @@ public class LockOnSystem : MonoBehaviour
 		{
 			isLockOn = true;
 			elapsedTime = 1;
+
+			Transform trans = target.transform;
+			trans.position += new Vector3(0, 0.5f, 0);
+
+			weapon.transform.LookAt(trans);
 		}
 
-		if (target != null)
-		{
-			weapon.transform.LookAt(target.transform);
-		}
 	}
 
 
@@ -68,12 +69,22 @@ public class LockOnSystem : MonoBehaviour
 	{
 		float search_radius = 10f;
 
+		//var hits = Physics.SphereCastAll(
+		//	player.transform.position,
+		//	search_radius,
+		//	player.transform.forward,
+		//	0.01f
+	
+		//).Select(h => h.transform.gameObject).ToList();
+
 		var hits = Physics.SphereCastAll(
 			player.transform.position,
 			search_radius,
 			player.transform.forward,
-			0.01f
-	
+			0.01f//,
+			//LayerMask.NameToLayer("Ignore Raycast")
+			//2
+
 		).Select(h => h.transform.gameObject).ToList();
 
 		hits = FilterTargetObject(hits);
