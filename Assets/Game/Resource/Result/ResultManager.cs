474222d0e27acb24e5f2db932a49d11571025ef7
Cysa8010 <hishoslash@gmail.com>
2021-01-22 00:22:58 +0900

    リザルトのUI追加

---
+++ Assets/Game/Resource/Result/ResultManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetResultData(int coin, int minu, int sec, int msec)
+	{
+        this.coin.SetCoin(coin);
+        this.clearTime.SetTime(minu, sec, msec);
+	}
+
+    [SerializeField] private S_CoinScore coin = null;
+    [SerializeField] private S_TimeScore clearTime = null;
+}
