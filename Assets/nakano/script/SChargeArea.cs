92ad52b55c4806d8b1fd4cdfce655b4bc3a73c45
makoto <mary1208@users.noreply.github.com>
2021-01-22 01:34:36 +0900

    エネルギーチャージエリア作成

---
+++ Assets/nakano/script/SChargeArea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SChargeArea : MonoBehaviour
+{
+    GameObject BulletEnergyObject;
+    SBulletEnergy BulletEnergyScript;
+
+    public float ChargeValue;
+    private bool isCharged;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BulletEnergyObject = GameObject.Find("gage");
+        BulletEnergyScript = BulletEnergyObject.GetComponent<SBulletEnergy>();
+        isCharged = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AddCharge()
+    {
+        BulletEnergyScript.energy += ChargeValue;
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AddCharge();
+        }
+
+    }
+
+
+}
