c56cba737eebdfc9ad39229922f6ccdd3c24d691
Shino720s <masakazu990311@gmail.com>
2021-01-21 17:33:22 +0900

    リザルトシーンの中身作りました。

---
+++ Assets/shinoSagyou/Script/S_CoinScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_CoinScore : MonoBehaviour
+{
+    // UI Text指定用
+    public Text CoinText;
+    // 獲得コイン受け皿
+    private int gottencoin;
+ 
+    // Use this for initialization
+    void Start () {
+        gottencoin = 0;
+    }
+     
+    // Update is called once per frame
+    void Update () {
+        CoinText.text = string.Format("{0:0000} coin", gottencoin);
+    }
+}
