6eaae065146ba68aae3409437f680da2300a1473
Cysa8010 <hishoslash@gmail.com>
2021-01-21 22:14:48 +0900

    Menuシーンのボタン実装

--- Assets/Game/Resource/SButtonCtrl.cs
+++ Assets/Game/Resource/SButtonCtrl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class SMissionStart : MonoBehaviour
+public class SButtonCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,28 +15,17 @@ public class SMissionStart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Fire2"))
-        //{
-        //    if (!scene)
-        //    {
-        //        Debug.Log("Null");
-        //        return;
-        //    }
-        //    scene.ChangeScene(0);
-        //}
-        if(eventSystem.currentSelectedGameObject.gameObject.GetInstanceID()==button.gameObject.GetInstanceID())
-		{
+        if (eventSystem.currentSelectedGameObject.gameObject.GetInstanceID() == button.gameObject.GetInstanceID())
+        {
             RectTransform rt = transform as RectTransform;
             rt.localScale = new Vector3(1.1f, 1.1f, 1);
-		}
+        }
         else
-		{
+        {
             RectTransform rt = transform as RectTransform;
             rt.localScale = new Vector3(1f, 1f, 1);
         }
-        
     }
-
     public void OnClick()
     {
         if (!scene)
@@ -44,11 +33,11 @@ public class SMissionStart : MonoBehaviour
             Debug.Log("Null");
             return;
         }
-        scene.ChangeScene(1);
-
+        scene.ChangeScene(sceneIndex);
     }
 
     [SerializeField] private SSceneManager scene = null;
     [SerializeField] private Button button = null;
     [SerializeField] private EventSystem eventSystem = null;
+    [SerializeField] private int sceneIndex = 0;
 }
